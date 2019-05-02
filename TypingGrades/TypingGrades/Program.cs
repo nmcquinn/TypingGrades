86b3c34dfda23f3722a50e1824ebda379e7798a5
nmcquinn <35936306+nmcquinn@users.noreply.github.com>
2019-05-02 09:22:00 -0400

    Add files via upload

---
+++ TypingGrades/TypingGrades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+
+namespace TypingGrades
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputString;
+            int wordsTyped;
+
+            Write("Enter the number of words typed per minute: ");
+            inputString = ReadLine();
+            wordsTyped = Convert.ToInt32(inputString);
+
+
+            int[] gradesLowRange = { 0, 16, 31, 51, 76 };
+
+            string[] letterGrade = { "F", "D", "C", "B", "A" };
+
+            for (int i = 0; i < gradesLowRange.Length; i++)
+            {
+                if (wordsTyped >= gradesLowRange[i] && wordsTyped < gradesLowRange[i + 1])
+                {
+
+                    WriteLine("Typing {0} words per minute: Grade {1}", wordsTyped, letterGrade[i]);
+                    ReadKey();
+                }
+            }
+        }
+    }
+}
+
+    
